0d728a37c6672226a329d86f481ac5c2562a0fa1
victor <victorpedr879@gmail.com>
2023-07-08 23:21:01 -0300

    build: instaling react-helmet and adding favicon

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import Navbar from "./components/NavBar";
 import Packages from "./components/Packages";
 import Weekend from "./components/Weekend";
 
+import { Helmet } from "react-helmet";
+
 function App() {
   return (
     <>
+    <Helmet>
+      <title>Travel</title>
+    </Helmet>
       <Navbar />
       <Home />
       <Destinations />

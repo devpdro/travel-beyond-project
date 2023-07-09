17f429bbe54443e71e9bbd5532e1ac285e040fa3
victor <victorpedr879@gmail.com>
2023-07-08 22:41:23 -0300

    feat: section packages finished

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -16,12 +16,25 @@ function Packages() {
           <div key={item.id} className={styles.images_box}>
             <div>
               <img
-                key={item.id}
                 src={item.img}
-                alt="Imagens mostrando os destinos"
+                alt="Imagens mostrando os lugares dos pacotes de viagens"
               />
-              <h3>{item.cardHeading}</h3>
-              <p>{item.packages}</p>
+              <h3>{item.state}</h3>
+              <p>{item.country}</p>
+              <h4>
+                {item.type}
+                <span>{item.price}</span>
+              </h4>
+              <p>{item.paragraph}</p>
+              <input
+                style={{
+                  backgroundColor: item.bg,
+                  color: item.textColor,
+                }}
+                className={styles.btn_details}
+                type="submit"
+                value={item.text}
+              />
             </div>
           </div>
         ))}
